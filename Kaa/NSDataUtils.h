10684eaa1c44613bf3343db2b07bc4ea8969d847
agutyro <am1k@icloud.com>
2015-10-05 14:10:09 +0300

    EndpointObjectHash description fix. Added NSDataUtils

---
+++ Kaa/NSDataUtils.h
@@ -0,0 +1,16 @@
+//
+//  NSDataUtils.h
+//  Kaa
+//
+//  Created by Aleksey Gutyro on 05.10.15.
+//  Copyright © 2015 CYBERVISION INC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDataUtils : NSObject
+
+
++ (NSString *)toHex:(NSData*)data;
+
+@end
