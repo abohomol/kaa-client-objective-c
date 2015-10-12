1948756e7594402f262003e07fb941a991bcc913
agutyro <am1k@icloud.com>
2015-10-12 17:45:58 +0300

    Add basic subsequent test for MessageEncoderDecoder

--- KaaTests/MessageEncoderDecoderTests.m
+++ KaaTests/MessageEncoderDecoderTests.m
@@ -72,6 +72,52 @@
     XCTAssertFalse([server verify:theifData withSignature:theifSignature]);
 }
 
+- (void)testBasicSubsequentTest {
+    NSString *message = [NSString stringWithFormat:@"secret%i", arc4random()];
+    NSData *messageData = [message dataUsingEncoding:NSUTF8StringEncoding];
+    
+    MessageEncoderDecoder *client = [[MessageEncoderDecoder alloc] initWithKeyPair:self.clientPair
+                                                             andRemotePublicKeyRef:[self.serverPair getPublicKeyRef]];
+    MessageEncoderDecoder *client2 = [[MessageEncoderDecoder alloc] initWithKeyPair:self.theifPair
+                                                             andRemotePublicKeyRef:[self.serverPair getPublicKeyRef]];
+    MessageEncoderDecoder *server = [[MessageEncoderDecoder alloc] initWithKeyPair:self.serverPair
+                                                             andRemotePublicKeyRef:[self.clientPair getPublicKeyRef]];
+    
+    NSData *secretData = [client encodeData:messageData];
+    NSData *signature = [client sign:secretData];
+    NSData *encodedSessionKey = [client getEncodedSessionKey];
+    XCTAssertTrue([server verify:secretData withSignature:signature]);
+    
+    NSData *decodedData = [server decodeData:secretData withEncodedKey:encodedSessionKey];
+    NSString *decodedSecret = [[NSString alloc] initWithData:decodedData encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([message isEqualToString:decodedSecret]);
+    
+    [server setRemotePublicKeyRef:[self.theifPair getPublicKeyRef]];
+
+    NSData *secretData2 = [client2 encodeData:messageData];
+    NSData *signature2 = [client2 sign:secretData2];
+    NSData *encodedSessionKey2 = [client2 getEncodedSessionKey];
+    XCTAssertTrue([server verify:secretData2 withSignature:signature2]);
+    
+    NSData *decodedData2 = [server decodeData:secretData2 withEncodedKey:encodedSessionKey2];
+    NSString *decodedSecret2 = [[NSString alloc] initWithData:decodedData2 encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([message isEqualToString:decodedSecret2]);
+}
+
+- (void)testBasicUpdate {
+    MessageEncoderDecoder *client = [[MessageEncoderDecoder alloc] initWithKeyPair:self.clientPair
+                                                             andRemotePublicKeyRef:[self.serverPair getPublicKeyRef]];
+    
+    BOOL test = [client getPrivateKey] != nil;
+    XCTAssertTrue(test);
+    test = [client getPublicKey] != nil;
+    XCTAssertTrue(test);
+    test = [client getRemotePublicKey] != nil;
+    XCTAssertTrue(test);
+
+}
+
+
 - (NSData *)generateTag {
     int randomInt = arc4random();
     return [NSData dataWithBytes:&randomInt length:sizeof(randomInt)];
