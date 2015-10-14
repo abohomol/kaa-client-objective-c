67f85017574fce96896c032efb7478e88767ff5d
Aleksey Gutyro <am1k19930322@gmail.com>
2015-10-14 14:41:27 +0300

    Add signetureTest and verifyResponseFailureTest in AbstractHttpClientTest

--- KaaTests/AbstractHttpClientTest.m
+++ KaaTests/AbstractHttpClientTest.m
@@ -11,6 +11,7 @@
 #import <Foundation/Foundation.h>
 #import "KeyUtils.h"
 #import "KeyPair.h"
+#import "MessageEncoderDecoder.h"
 
 @interface TestHttpClient : AbstractHttpClient
 
@@ -40,13 +41,34 @@
 
 @interface AbstractHttpClientTest : XCTestCase
 
+@property (nonatomic,strong) KeyPair *clientPair;
+
+@property (nonatomic,strong) NSData *serverPrivateTag;
+@property (nonatomic,strong) NSData *serverPublicTag;
+@property (nonatomic,strong) KeyPair *serverPair;
+
 @end
 
 @implementation AbstractHttpClientTest
 
+- (void) setUp {
+    [super setUp];
+    self.clientPair = [KeyUtils generateKeyPair];
+    
+    self.serverPrivateTag = [self generateTag];
+    self.serverPublicTag = [self generateTag];
+    self.serverPair = [KeyUtils generateKeyPairWithPrivateTag:self.serverPrivateTag andPublicTag:self.serverPublicTag];
+}
+
+- (void) tearDown {
+    [super tearDown];
+    [KeyUtils deleteExistingKeyPair];
+    [KeyUtils removeKeyByTag:self.serverPrivateTag];
+    [KeyUtils removeKeyByTag:self.serverPublicTag];
+}
 
 - (void) testDisableVerification {
-    TestHttpClient *client = [[TestHttpClient alloc] initWith:@"test_url" privateKey:nil publicKey:nil remoteKey:nil];
+    TestHttpClient *client = [[TestHttpClient alloc] initWith:@"test_url" privateKey:nil publicKey:nil remoteKeyRef:nil];
     [client disableVerification];
     int a = 1; int b = 2; int c = 3;
     NSMutableData *body = [NSMutableData data];
@@ -58,4 +80,42 @@
     XCTAssertEqualObjects(body, [client verifyResponse:body signature:signature]);
 }
 
+- (void) testSignature {
+    TestHttpClient *client = [[TestHttpClient alloc] initWith:@"test_url" privateKey:[self.clientPair getPrivateKeyRef] publicKey:[self.clientPair getPublicKeyRef] remoteKeyRef:[self.serverPair getPublicKeyRef]];
+    
+    MessageEncoderDecoder *serverEncoder = [[MessageEncoderDecoder alloc] initWithKeyPair:self.serverPair andRemotePublicKeyRef:[self.clientPair getPublicKeyRef]];
+    
+    int a = 1; int b = 2; int c = 3;
+    NSMutableData *message = [NSMutableData data];
+    [message appendBytes:&a length:sizeof(a)];
+    [message appendBytes:&b length:sizeof(b)];
+    [message appendBytes:&c length:sizeof(c)];
+    
+    NSData *signature = [serverEncoder sign:message];
+    XCTAssertEqualObjects(message, [client verifyResponse:message signature:signature]);
+}
+
+- (void) testVerifyResponseFailure {
+    
+    @try {
+        TestHttpClient *client = [[TestHttpClient alloc] initWith:@"test_url" privateKey:[self.clientPair getPrivateKeyRef] publicKey:[self.clientPair getPublicKeyRef] remoteKeyRef:[self.serverPair getPublicKeyRef]];
+        
+        int a = 1; int b = 2; int c = 3;
+        NSMutableData *body = [NSMutableData data];
+        [body appendBytes:&a length:sizeof(a)];
+        [body appendBytes:&b length:sizeof(b)];
+        [body appendBytes:&c length:sizeof(c)];
+        
+        NSData *signature = [NSData dataWithData:body];
+        [client verifyResponse:body signature:signature];
+        XCTFail();
+    }
+    @catch (NSException *exception) {
+    }
+}
+
+- (NSData *)generateTag {
+    int randomInt = arc4random();
+    return [NSData dataWithBytes:&randomInt length:sizeof(randomInt)];
+}
 @end
