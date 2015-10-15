71abdfb288a300a6beffb294f1d07fc91b0c2796
abohomol <cb-abohomol@jawbone.com>
2015-10-15 15:51:31 +0300

    Replace NSLock with synchronized blocks

--- Kaa/MemLogStorage.m
+++ Kaa/MemLogStorage.m
@@ -28,8 +28,6 @@
 
 @property (nonatomic,strong) MemBucket *currentBucket;
 
-@property (atomic,strong) NSLock *bucketsLock;
-
 @end
 
 @implementation MemLogStorage
@@ -42,7 +40,6 @@
         self.maxBucketRecordCount = DEFAULT_MAX_BUCKET_RECORD_COUNT;
         self.bucketIdSeq = 0;
         self.buckets = [NSMutableDictionary dictionary];
-        self.bucketsLock = [[NSLock alloc] init];
     }
     return self;
 }
@@ -55,7 +52,6 @@
         self.maxBucketRecordCount = maxBucketRecordCount;
         self.bucketIdSeq = 0;
         self.buckets = [NSMutableDictionary dictionary];
-        self.bucketsLock = [[NSLock alloc] init];
     }
     return self;
 }
@@ -68,7 +64,6 @@
         self.maxBucketRecordCount = bucketRecordCount;
         self.bucketIdSeq = 0;
         self.buckets = [NSMutableDictionary dictionary];
-        self.bucketsLock = [[NSLock alloc] init];
     }
     return self;
 }
@@ -89,24 +84,24 @@
         [NSException raise:NSInvalidArgumentException format:@"Record size(%li) is bigger than max bucket size(%li)!",
          (long)[record getSize], (long)self.maxBucketSize];
     }
-    [self.bucketsLock tryLock];
-    if (self.consumedVolume + [record getSize] > self.maxStorageSize) {
-        [NSException raise:@"IllegalStateException" format:@"Storage is full!"];
+    @synchronized(self.buckets) {
+        if (self.consumedVolume + [record getSize] > self.maxStorageSize) {
+            [NSException raise:@"IllegalStateException" format:@"Storage is full!"];
+        }
+        if (!self.currentBucket || self.currentBucket.state != MEM_BUCKET_STATE_FREE) {
+            self.currentBucket = [[MemBucket alloc] initWithId:self.bucketIdSeq++ maxSize:self.maxBucketSize maxRecordCount:self.maxBucketRecordCount];
+            [self.buckets setObject:self.currentBucket forKey:[NSNumber numberWithLong:self.currentBucket.bucketId]];
+        }
+        if (![self.currentBucket addRecord:record]) {
+            DDLogVerbose(@"%@ Current bucket is full. Creating new one.", TAG);
+            self.currentBucket.state = MEM_BUCKET_STATE_FULL;
+            self.currentBucket = [[MemBucket alloc] initWithId:self.bucketIdSeq++ maxSize:self.maxBucketSize maxRecordCount:self.maxBucketRecordCount];
+            [self.buckets setObject:self.currentBucket forKey:[NSNumber numberWithLong:self.currentBucket.bucketId]];
+            [self.currentBucket addRecord:record];
+        }
+        self.recordCount++;
+        self.consumedVolume += [record getSize];
     }
-    if (!self.currentBucket || self.currentBucket.state != MEM_BUCKET_STATE_FREE) {
-        self.currentBucket = [[MemBucket alloc] initWithId:self.bucketIdSeq++ maxSize:self.maxBucketSize maxRecordCount:self.maxBucketRecordCount];
-        [self.buckets setObject:self.currentBucket forKey:[NSNumber numberWithLong:self.currentBucket.bucketId]];
-    }
-    if (![self.currentBucket addRecord:record]) {
-        DDLogVerbose(@"%@ Current bucket is full. Creating new one.", TAG);
-        self.currentBucket.state = MEM_BUCKET_STATE_FULL;
-        self.currentBucket = [[MemBucket alloc] initWithId:self.bucketIdSeq++ maxSize:self.maxBucketSize maxRecordCount:self.maxBucketRecordCount];
-        [self.buckets setObject:self.currentBucket forKey:[NSNumber numberWithLong:self.currentBucket.bucketId]];
-        [self.currentBucket addRecord:record];
-    }
-    self.recordCount++;
-    self.consumedVolume += [record getSize];
-    [self.bucketsLock unlock];
     
     DDLogVerbose(@"%@ Added a new log record to bucket with id [%li]", TAG, (long)[self.currentBucket bucketId]);
 }
@@ -119,56 +114,56 @@
     }
     LogBlock *result = nil;
     MemBucket *bucketCandidate = nil;
-    [self.bucketsLock lock];
-    for (MemBucket *bucket in self.buckets.allValues) {
-        if (bucket.state == MEM_BUCKET_STATE_FREE) {
-            bucketCandidate = bucket;
+    @synchronized(self.buckets) {
+        for (MemBucket *bucket in self.buckets.allValues) {
+            if (bucket.state == MEM_BUCKET_STATE_FREE) {
+                bucketCandidate = bucket;
+            }
+            if (bucket.state == MEM_BUCKET_STATE_FULL) {
+                bucket.state = MEM_BUCKET_STATE_PENDING;
+                bucketCandidate = bucket;
+                break;
+            }
         }
-        if (bucket.state == MEM_BUCKET_STATE_FULL) {
-            bucket.state = MEM_BUCKET_STATE_PENDING;
-            bucketCandidate = bucket;
-            break;
-        }
-    }
-    if (bucketCandidate) {
-        self.consumedVolume -= [bucketCandidate getSize];
-        self.recordCount -= [bucketCandidate getCount];
-        if (bucketCandidate.state == MEM_BUCKET_STATE_FREE) {
-            DDLogVerbose(@"%@ Only a bucket with state FREE found: [%li]. Changing its state to PENDING",
-                         TAG, (long)bucketCandidate.bucketId);
-            bucketCandidate.state = MEM_BUCKET_STATE_PENDING;
-        }
-        if ([bucketCandidate getSize] <= blockSize && [bucketCandidate getCount] <= batchCount) {
-            result = [[LogBlock alloc] initWithBlockId:bucketCandidate.bucketId andRecords:bucketCandidate.records];
-            DDLogDebug(@"%@ Return record block with records count: [%li]", TAG, (long)[bucketCandidate getCount]);
-        } else {
-            DDLogDebug(@"%@ Shrinking bucket %@ to new size: [%li] and count: [%li]", TAG, bucketCandidate, (long)blockSize, (long)batchCount);
-            NSArray *overSized = [bucketCandidate shrinkToSize:blockSize newCount:batchCount];
-            result = [[LogBlock alloc] initWithBlockId:bucketCandidate.bucketId andRecords:bucketCandidate.records];
-            for (LogRecord *logRecord in overSized) {
-                [self addLogRecord:logRecord];
+        if (bucketCandidate) {
+            self.consumedVolume -= [bucketCandidate getSize];
+            self.recordCount -= [bucketCandidate getCount];
+            if (bucketCandidate.state == MEM_BUCKET_STATE_FREE) {
+                DDLogVerbose(@"%@ Only a bucket with state FREE found: [%li]. Changing its state to PENDING",
+                             TAG, (long)bucketCandidate.bucketId);
+                bucketCandidate.state = MEM_BUCKET_STATE_PENDING;
+            }
+            if ([bucketCandidate getSize] <= blockSize && [bucketCandidate getCount] <= batchCount) {
+                result = [[LogBlock alloc] initWithBlockId:bucketCandidate.bucketId andRecords:bucketCandidate.records];
+                DDLogDebug(@"%@ Return record block with records count: [%li]", TAG, (long)[bucketCandidate getCount]);
+            } else {
+                DDLogDebug(@"%@ Shrinking bucket %@ to new size: [%li] and count: [%li]", TAG, bucketCandidate, (long)blockSize, (long)batchCount);
+                NSArray *overSized = [bucketCandidate shrinkToSize:blockSize newCount:batchCount];
+                result = [[LogBlock alloc] initWithBlockId:bucketCandidate.bucketId andRecords:bucketCandidate.records];
+                for (LogRecord *logRecord in overSized) {
+                    [self addLogRecord:logRecord];
+                }
             }
         }
     }
-    [self.bucketsLock unlock];
     return result;
 }
 
 - (void)removeRecordBlock:(NSInteger)blockId {
     DDLogVerbose(@"%@ Removing record block with id [%li]", TAG, (long)blockId);
-    [self.bucketsLock lock];
-    [self.buckets removeObjectForKey:[NSNumber numberWithLong:blockId]];
-    [self.bucketsLock unlock];
+    @synchronized(self.buckets) {
+        [self.buckets removeObjectForKey:[NSNumber numberWithLong:blockId]];
+    }
 }
 
 - (void)notifyUploadFailed:(NSInteger)blockId {
     DDLogVerbose(@"%@ Upload of record block [%li] failed", TAG, (long)blockId);
-    [self.bucketsLock lock];
-    MemBucket * bucket = [self.buckets objectForKey:[NSNumber numberWithLong:blockId]];
-    bucket.state = MEM_BUCKET_STATE_FULL;
-    self.consumedVolume += [bucket getSize];
-    self.recordCount += [bucket getCount];
-    [self.bucketsLock unlock];
+    @synchronized(self.buckets) {
+        MemBucket * bucket = [self.buckets objectForKey:[NSNumber numberWithLong:blockId]];
+        bucket.state = MEM_BUCKET_STATE_FULL;
+        self.consumedVolume += [bucket getSize];
+        self.recordCount += [bucket getCount];
+    }
 }
 
 - (void)close {
